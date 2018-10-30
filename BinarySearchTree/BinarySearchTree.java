78f13962dc42e87cb7e0500b69fa7b1c6d97a0f9
Akash Anand <36139806+akash9920@users.noreply.github.com>
2018-10-29 22:51:46 -0400

    Java resources file

---
+++ BinarySearchTree/BinarySearchTree.java
@@ -0,0 +1,7 @@
+import BinarySearchTree.Node;
+
+
+class BinarySearchTree{
+
+
+}
---
+++ BinarySearchTree/Node.java
@@ -0,0 +1,16 @@
+class Node  {
+
+    private Comparable data;
+    private Node left;
+    private Node right;
+
+
+    public Node(Comparable NodeValue){
+
+        this.data = NodeValue;
+        this.left = null;
+        this.right = null;
+    }
+
+
+}
