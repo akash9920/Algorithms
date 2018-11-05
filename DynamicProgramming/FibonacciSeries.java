f626ae122973970346f10c82298e0cb208cdc3f0
Akash Anand <36139806+akash9920@users.noreply.github.com>
2018-11-05 18:30:34 -0500

    mistakeDP.readme updated

--- DynamicProgramming/FibonacciSeries.java
+++ DynamicProgramming/FibonacciSeries.java
@@ -1,28 +1,44 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class FibonacciSeries {
 
+  static   Map<Integer,Integer> fibMap  = new HashMap<>();
 
     public static void main(String args[] ){
 
+            int n = 10;
 
-            int i = fin(n);
+           // FibonacciSeries f = new FibonacciSeries();
+            int i = fib(n);
 
             System.out.println(" the value of the fib is " + i);
-
 
     }
 
 
+public static int fib(int n){
 
-    public int fib(int n){
+   
+    return startfib(n);
+}
 
 
+    public static int startfib(int n){
+
+        
         if(n<=1){
             return 1;
         }
 
-        return fib(n-1) + fib(n-2);
+        if(fibMap.containsKey(n)){
 
+            return fibMap.get(n);
+        }
 
+        int value = fib(n-1) + fib(n-2);
+        fibMap.put(n, value);
+        return value;
     }
 
 
---
+++ DynamicProgramming/MistakesDP.java
@@ -0,0 +1,20 @@
+
+class MistakesDP{
+
+    Fibonacci
+/*
+for the fibonacci series question, hashmap edclaration. only on the left side. 
+*/
+
+//error code line: 
+static   Map<Integer,Integer> fibMap  = new HashMap<Integer,Integer>();
+
+//corrected code:
+
+static   Map<Integer,Integer> fibMap  = new HashMap<Integer,Integer>();
+
+
+
+}
+
+
