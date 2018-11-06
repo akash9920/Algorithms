2af25728ee4acac0d9f6b5ac5f5e8f35bfcd4414
Akash Anand <36139806+akash9920@users.noreply.github.com>
2018-11-05 23:32:24 -0500

    junk files removed

--- DynamicProgramming/MistakesDP.java
+++ DynamicProgramming/MistakesDP.java
@@ -1,17 +1,19 @@
 
 class MistakesDP{
 
-    Fibonacci
+   String Fibonacci;
 /*
 for the fibonacci series question, hashmap edclaration. only on the left side. 
 */
 
 //error code line: 
-static   Map<Integer,Integer> fibMap  = new HashMap<Integer,Integer>();
+static   Map<Integer,Integer> fibMap1  = new HashMap<Integer,Integer>();
 
 //corrected code:
 
 static   Map<Integer,Integer> fibMap  = new HashMap<Integer,Integer>();
+
+
 
 
 
